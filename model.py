ba1ef740be03a76eb09df0dd5350db7aa2bfc150
Bilal Ahmad <1704922@brunel.ac.uk>
2022-05-09 02:05:52 +0100

    Created functions for model to transfer to aan account and to retrieve transactions

--- model.py
+++ model.py
@@ -75,3 +75,32 @@ class BankDatabase():
         self.cursor.commit()
         self.cursor.close()
         return pulled_balance
+    
+    def transfer_to_account(self, sender_username, recipient_username, transfer_amount):
+        self.start_connection()
+        #retieves Account details for both sender and recipient 
+        sender_account = self.cursor.execute("SELECT AccountID, Balance FROM Account, User WHERE Account.UserID = User.UserID AND UserName ='" + sender_username + "';").fetchall()
+        self.cursor.commit()
+        recipient_account = self.cursor.execute("SELECT AccountID, Balance FROM Account, User WHERE Account.UserID = User.UserID AND UserName ='" + recipient_username + "';").fetchall()
+        self.cursor.commit()
+        date_and_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        #new record added for this Transaction
+        self.cursor.execute("INSERT INTO 'Transaction'(SenderID, RecipientID, Date, Amount) VALUES ('" + str(sender_account[0][0]) + "', '" + str(recipient_account[0][0]) + "', '" + date_and_time + "', " + str(transfer_amount) + ");")
+        self.cursor.commit()
+        #The link table is updated to replace the senders account ID with the recipients account ID. The update is applied to the same amount of records as the transfer amount.
+        self.cursor.execute("UPDATE AccountHasBambEuro SET AccountID = '" + str(recipient_account[0][0]) + "' WHERE rowid IN (SELECT rowid FROM AccountHasBambEuro WHERE AccountID ='" + str(sender_account[0][0]) + "' LIMIT " + str(transfer_amount) + ");" )
+        self.cursor.commit()
+        #The balance is updated for the sender and recipient based on the this transfer.
+        self.cursor.execute("UPDATE Account SET Balance = " + str(sender_account[0][1] - transfer_amount) + " WHERE AccountID = '" + str(sender_account[0][0]) + "';")
+        self.cursor.commit()
+        self.cursor.execute("UPDATE Account SET Balance = " + str(recipient_account[0][1] + transfer_amount) + " WHERE AccountID = '" + str(recipient_account[0][0]) + "';")
+        self.cursor.commit()
+        self.cursor.close()
+    
+    def retrieve_transactions(self, username):
+        self.start_connection()
+        #Retrieves all transactions for given username - uses multiple select queries to retrieve names of sender and recipient
+        transactions = self.cursor.execute("SELECT (SELECT UserName FROM User, Account WHERE 'Transaction'.SenderID = Account.AccountID AND Account.UserID = User.UserID) as Sender, (SELECT UserName FROM User, Account WHERE 'Transaction'.RecipientID = Account.AccountID AND Account.UserID = User.UserID) as Recipient, Amount, Date FROM 'Transaction', Account, User WHERE ('Transaction'.SenderID = Account.AccountID OR 'Transaction'.RecipientID = Account.AccountID) AND Account.UserID = User.UserID AND User.UserName = '" + username + "'  ORDER BY TransactionID DESC;").fetchall()
+        self.cursor.commit()
+        self.cursor.close()
+        return transactions
--- model_test.py
+++ model_test.py
@@ -32,3 +32,23 @@ class model_test(unittest.TestCase):
         actual_value = bank_database.retrieve_account_balance("test_user01")
         expected_value = 100
         self.assertEqual(expected_value,actual_value)
+    
+    def test_transfer_to_account(self):
+        bank_database = BankDatabase()
+        sender = "test_user01"
+        recipient = "test_user02"
+        bank_database.transfer_to_account(sender, recipient, 20)
+        expectedValue = 80
+        connection = sqlite3.connect("BambEuroBank.db")
+        cursor = connection.cursor()
+        user_balance = cursor.execute("SELECT COUNT(ID) FROM AccountHasBambEuro, Account, User WHERE AccountHasBambEuro.AccountID = Account.AccountID AND Account.UserID = User.UserID AND User.UserName = '" + sender + "';").fetchall()[0][0]
+        cursor.close()
+        self.assertEquals(expectedValue, user_balance)
+        bank_database.transfer_to_account(recipient, sender, 20)
+    
+    def test_retrieve_transactions(self):
+        bank_database = BankDatabase()
+        transactions = bank_database.retrieve_transactions("test_user03")
+        expected_value = 2
+        actual_value = len(transactions)
+        self.assertEqual(expected_value, actual_value)
